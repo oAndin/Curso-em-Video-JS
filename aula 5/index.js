8d2a745f69ca2993a9f5cd3647801ff6e2803bb8
oAndin <00andherson@gmail.com>
2023-06-03 16:10:18 -0300

    O que é a realidade

--- aula 5/index.js
+++ aula 5/index.js
@@ -19,19 +19,17 @@ window.alert(`É um prazer ${nome}`); // O ponto value estava me seguindo até a
 
 let n1 = Number.parseInt(window.prompt("Digite o primeiro número de verdade:"));
 let n2 = Number.parseInt(window.prompt("Digite o segundo número de verdade:"));
-sum1();
 function sum1() {
-    if (n1 != "none" && n2 != "none") {
-        
-        const soma1 = n1 + n2;
+    if (n1 !== "none" && n2 !== "none") {
+        let soma1 = n1 + n2;
         return soma1;
     }
     else {
         window.alert("Você não colocou nenhum dado no campo!");
     };
 };
-
-const resultado = sum1.value;
+sum1();
+const resultado = soma1;
 console.log(resultado);
 console.log(n1);
 console.log(n2);
